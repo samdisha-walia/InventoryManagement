3c7539c51a854acb554409bd3a27fdc9cb565b8c
Samdisha Walia <samdishaahluwalia2003@gmail.com>
2024-05-12 22:59:48 +0530

    Added files

---
+++ Dashboard/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include 
+from . import views
+
+urlpatterns = [
+    path('Home/', views.Home, name = 'Home'),
+    path('Inventory/', views.Inventory, name = 'Inventory'),
+    path('Orders/', views.Orders, name = 'Orders'),
+    path('Analytics/', views.Analytics, name = 'Analytics'),
+    path('Settings/', views.Settings, name = 'Settings'),
+    path('Users/', views.Users, name = 'Users'),
+    path('Help/', views.Help, name = 'Help'),
+    path('Notifications/', views.Notifications, name = 'Notifications'),
+    path('Logout/', views.Logout, name = 'Logout'),
+]
---
+++ Dashboard/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Home(request):
+    return render(request, 'dash/Home.html')
+
+def Inventory(request):
+    return render(request, 'dash/Inventory.html')
+
+def Orders(request):
+    return render(request, 'dash/Orders.html')
+
+def Analytics(request):
+    return render(request, 'dash/Analytics.html')
+
+def Settings(request):
+    return render(request, 'dash/Settings.html')
+
+def Users(request):
+    return render(request, 'dash/Users.html')
+
+def Help(request):
+    return render(request, 'dash/Help.html')
+
+def Notifications(request):
+    return render(request, 'dash/Notifications.html')
+
+def Logout(request):
+    return render(request, 'dash/Logout.html')
