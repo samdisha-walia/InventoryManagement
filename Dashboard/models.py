d9d50281c50d6518c1c65d28c83e797ae9fe0f53
Samiksha Walia <samikshawalia6903@gamil.com>
2024-05-26 11:07:06 +0530

    superuser

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY =(
+    ('Stationary','Stationary'),
+    ('Electronics','Electronics'),
+    ('Food','Food'),
+)
+
+class Product(models.Model):
+    name= models.CharField(max_length=100,null=True)
+    category=models.CharField(max_length=20,choices=CATEGORY,null=True)
+    quantity=models.PositiveIntegerField(null=True)
+
+
+    
+
+   # class Meta:
+     #   verbose_name = _("")
+#        verbose_name_plural = _("s")
+
+   # def __str__(self):
+   #     return self.name
+
+  #  def get_absolute_url(self):
+   #     return reverse("_detail", kwargs={"pk": self.pk})
