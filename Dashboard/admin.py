1238abe4fe9c08c076d9cbd4b0ac9711f50fd1cb
Samiksha Walia <samikshawalia6903@gamil.com>
2024-05-27 22:23:48 +0530

    created tables

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Order
+from django.contrib.auth.models import Group
+
+
+admin.site.site_header = 'Inventory Management'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ('name', 'category','quantity')
+    list_filter= ('name', 'category',)
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY =(
@@ -21,7 +22,19 @@ class Product(models.Model):
 #        verbose_name_plural = _("s")
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name}-{self.quantity}'
 
   #  def get_absolute_url(self):
    #     return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null= True)
+    staff =  models.ForeignKey(User,on_delete=models.CASCADE,null= True)
+    order_quantity= models.PositiveIntegerField(null= True)
+    date= models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
+    
+    
